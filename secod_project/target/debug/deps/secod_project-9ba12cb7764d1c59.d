ec5e9e5c9b5355a5e7676e865c7b19a4fe2c87e8
workshop748 <jacobpeacy11@gmail.com>
2024-01-18 21:46:59 -0600

    making file

---
+++ secod_project/target/debug/deps/secod_project-9ba12cb7764d1c59.d
@@ -0,0 +1,5 @@
+C:\Users\james\Documents\GitHub\first_Rust-Poject\secod_project\target\debug\deps\libsecod_project-9ba12cb7764d1c59.rmeta: src\main.rs
+
+C:\Users\james\Documents\GitHub\first_Rust-Poject\secod_project\target\debug\deps\secod_project-9ba12cb7764d1c59.d: src\main.rs
+
+src\main.rs:
